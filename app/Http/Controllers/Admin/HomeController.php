91a4e2e78e36e29246ef411e04f9c4f823023243
Laravel Daily <info@laraveldaily.com>
2022-05-19 07:04:36 +0000

    QuickAdminPanel automatic commit

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -2,45 +2,10 @@
 
 namespace App\Http\Controllers\Admin;
 
-use LaravelDaily\LaravelCharts\Classes\LaravelChart;
-
 class HomeController
 {
     public function index()
     {
-        $settings1 = [
-            'chart_title'           => 'Dealer Commission Report',
-            'chart_type'            => 'latest_entries',
-            'report_type'           => 'group_by_date',
-            'model'                 => 'App\Models\MlmLevel',
-            'group_by_field'        => 'created_at',
-            'group_by_period'       => 'day',
-            'aggregate_function'    => 'count',
-            'filter_field'          => 'created_at',
-            'filter_days'           => '7',
-            'group_by_field_format' => 'm/d/Y H:i:s',
-            'column_class'          => 'col-md-12',
-            'entries_number'        => '5',
-            'fields'                => [
-                'id'             => '',
-                'user'           => 'name',
-                'current_plan'   => 'name',
-                'children_count' => '',
-            ],
-            'translation_key' => 'mlmLevel',
-        ];
-
-        $settings1['data'] = [];
-        if (class_exists($settings1['model'])) {
-            $settings1['data'] = $settings1['model']::latest()
-                ->take($settings1['entries_number'])
-                ->get();
-        }
-
-        if (!array_key_exists('fields', $settings1)) {
-            $settings1['fields'] = [];
-        }
-
-        return view('home', compact('settings1'));
+        return view('home');
     }
 }
